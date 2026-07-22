2b62b82c576c16a57b3afd7860deace9b528fef6
vijaybore <vijaybore05@gmail.com>
2026-07-22 12:51:10 +0530

    playstore ready

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 val keystorePropertiesFile = rootProject.file("key.properties")
@@ -25,7 +26,7 @@ android {
 
     defaultConfig {
         applicationId = "com.mykirana.app"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
